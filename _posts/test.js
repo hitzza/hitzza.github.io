92b76af71dd520d0d9b8cd53cff52c598bce9ae2
hitzza <cslzh4433@gmail.com>
2022-09-22 19:28:31 +0900

    hoisting post function2

---
+++ _posts/test.js
@@ -0,0 +1,12 @@
+function sum(x,y){
+    return x+y;
+}
+var a = sum(1,2);
+console.log(a);
+var sum = function(x,y){
+    return x + '+' + y + '=' + (x+y);
+}
+
+var c = sum(1,2);
+
+console.log(c);
